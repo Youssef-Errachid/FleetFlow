4f1965d5926a8a06b100f061edbb7b130ddc202c
Youssef <yousseferrachid@gmail.com>
2026-06-02 15:00:09 +0100

    add password hashing and validation to User entity

--- src/main/java/org/fleetflow/fleetflow/entity/User.java
+++ src/main/java/org/fleetflow/fleetflow/entity/User.java
@@ -5,6 +5,7 @@ import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.Collection;
 import java.util.List;
@@ -48,4 +49,33 @@ public class User implements UserDetails {
     @Override
     public boolean isEnabled() { return true; }
 
+    public void setHashedPassword(String password) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        this.password = encoder.encode(password);
+    }
+
 }
+
+git add pom.xml
+git commit -m " add JWT dependencies (jjwt)"
+
+git add src/main/java/org/fleetflow/fleetflow/entity/User.java
+git commit -m " add password hashing and validation to User entity"
+
+git add src/main/java/org/fleetflow/fleetflow/dto/AuthRequest.java
+git commit -m " create AuthRequest DTO with validation"
+
+git add src/main/java/org/fleetflow/fleetflow/dto/AuthResponse.java
+git commit -m " create AuthResponse and AuthRequest DTO for API "
+
+git add src/main/java/org/fleetflow/fleetflow/repository/UserRepository.java
+git commit -m " create UserRepository"
+
+git add src/main/java/org/fleetflow/fleetflow/security/
+git commit -m " add JWT utilities and security configuration"
+
+git add src/main/java/org/fleetflow/fleetflow/service/AuthService.java
+git commit -m " create AuthService with registration and login logic"
+
+git add src/main/java/org/fleetflow/fleetflow/controller/AuthController.java
+git commit -m " create AuthController with authentication endpoints"
