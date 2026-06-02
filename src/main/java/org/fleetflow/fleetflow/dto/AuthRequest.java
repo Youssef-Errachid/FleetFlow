d2b87868b14286adf60fb8da40002cb3aad0168c
Youssef <yousseferrachid@gmail.com>
2026-06-02 15:00:11 +0100

    create AuthRequest DTO with validation

---
+++ src/main/java/org/fleetflow/fleetflow/dto/AuthRequest.java
@@ -0,0 +1,30 @@
+// AuthRequest.java
+package org.fleetflow.fleetflow.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import org.fleetflow.fleetflow.entity.Role;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AuthRequest {
+
+    @NotBlank(message = "user name is required")
+    private String username;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email must be valid")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    private String password;
+
+    @NotBlank(message = "The Role is required")
+    private Role role;
+}
