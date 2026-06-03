65275f5e67432848a23a737fad809bcb679f5521
Youssef <yousseferrachid@gmail.com>
2026-06-03 12:17:42 +0100

    secure all endpoint of client

--- src/main/java/org/fleetflow/fleetflow/controller/ClientController.java
+++ src/main/java/org/fleetflow/fleetflow/controller/ClientController.java
@@ -8,6 +8,7 @@ import org.fleetflow.fleetflow.dto.ClientDTO;
 import org.fleetflow.fleetflow.service.ClientService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,17 +21,21 @@ public class ClientController {
 
     private final ClientService clientService;
 
+    @PreAuthorize("hasAnyRole('ADMIN','CLIENT')")
     @PostMapping
     @Operation(summary = "Ajouter un client")
     public ResponseEntity<ClientDTO> ajouter(@Valid @RequestBody ClientDTO dto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(clientService.ajouterClient(dto));
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{id}")
     @Operation(summary = "Modifier un client")
     public ResponseEntity<ClientDTO> modifier(@Valid @PathVariable Long id, @RequestBody ClientDTO dto) {
         return ResponseEntity.ok(clientService.modifierClient(id, dto));
     }
+
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{id}")
     @Operation(summary = "Supprimer un client")
     public ResponseEntity<Void> supprimer(@PathVariable Long id) {
@@ -38,12 +43,14 @@ public class ClientController {
         return ResponseEntity.noContent().build();
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN','CLIENT')")
     @GetMapping
     @Operation(summary = "Lister tous les clients")
     public ResponseEntity<List<ClientDTO>> lister() {
         return ResponseEntity.ok(clientService.listerClients());
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/{id}")
     @Operation(summary = "Obtenir un client par ID")
     public ResponseEntity<ClientDTO> getById(@PathVariable Long id) {
